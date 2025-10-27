b02c04b66da645efecf0353d7b80829409ce3ff8
Vencislav Belchev <vbelchev@hsdyn.com>
2025-10-27 17:17:40 +0200

    add initial test class for user story 5503, fixed a parametar in create tests

---
+++ SpacecraftManagementApp/Tests/SpacecraftTests/UserStory5503_Engine_Status_Updates.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation_Framework.SpacecraftManagementApp.Tests.SpacecraftTests
+{
+    public class UserStory5503_Engine_Status_Updates
+    {
+    }
+}
